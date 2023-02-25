026ca01a9c1606a4a66ed317edfad5ecf07295a4
Ege Özel <egexozel@gmail.com>
2023-02-26 00:04:24 +0300

    z

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,22 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-
 import Date from '../components/date';
 import { Tooltip } from '@nextui-org/react';
 import { Link } from "@nextui-org/react";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
+  const slicedPosts = allPostsData.slice(0, 4);
   return {
     props: {
-      allPostsData,
+      slicedPosts,
     },
   };
 }
 
 
-export default function Home({ allPostsData}) {
+export default function Home({ slicedPosts, allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -28,8 +28,8 @@ export default function Home({ allPostsData}) {
         <p>I hope you enjoy your time here!</p>
       </section>
       <section className={utilStyles.justifyContent}>
-      <section>
-        <h2 className={utilStyles.headingLg}>Ege's Internet Links</h2>
+        <section>
+          <h2 className={utilStyles.headingLg}>Ege's Internet Links</h2>
           <ul className={utilStyles.list}>
             <Tooltip content="https://mstdn.party/@ege" color="invert" placement="right" hideArrow><li className={utilStyles.listItem}><Link color="secondary" href='https://mstdn.party/@ege' isExternal>Mastodon</Link></li></Tooltip>
             <Tooltip content="https://twitter.com/egeozel" color="invert" placement='right' hideArrow><li className={utilStyles.listItem}><Link color="secondary" isExternal target="_blank" href="https://twitter.com/egeozel">Twitter</Link></li></Tooltip>
@@ -39,23 +39,22 @@ export default function Home({ allPostsData}) {
             <Tooltip content="https://www.last.fm/user/Egeozel" color="invert" placement='right' hideArrow><li className={utilStyles.listItem}><Link color="secondary" isExternal target="_blank" href="https://www.last.fm/user/Egeozel">Last.fm</Link></li></Tooltip>
             <Tooltip content="https://myanimelist.net/animelist/egell0" color="invert" placement='right' hideArrow><li className={utilStyles.listItem}><Link color="secondary" isExternal target="_blank" href="https://myanimelist.net/animelist/egell0">MyAnimeList</Link></li></Tooltip>
           </ul>
+        </section>
+        <section className={utilStyles.padding1px}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {slicedPosts.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link color="secondary" href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
       </section>
-      <section className={utilStyles.padding1px}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link color="secondary" href={`/posts/${id}`}>{title}</Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
-        </ul>
-      </section>
-      </section>
-      
     </Layout>
   );
 }

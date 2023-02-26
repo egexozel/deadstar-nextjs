a2e83e9dcf7acf6f53dc30abc67fc6f390f0e8e6
Ege Özel <egexozel@gmail.com>
2023-02-27 01:32:16 +0300

    added logo and a failed animation attempt

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,30 @@
 import Link from "next/link";
+import Image from 'next/image';
 import utilStyles from '../styles/utils.module.css';
+import Script from "next/script";
 
 const Nav = () => {
     return (
         <>
             <nav className={utilStyles.navbar}>
-                <Link href='/' className={utilStyles.navItem}>Home</Link>
-                <Link href='/archive' className={utilStyles.navItem}>Archive</Link>
-                <Link href='/about' className={utilStyles.navItem}>About</Link>
+                <Image
+                    priority
+                    src="/images/spacelogo.png"
+                    className={utilStyles.navLogo}
+                    height={40}
+                    width={40}
+                    alt="Logo of the website. Contains a black and white illustration of a space station inside a crest."
+                />
+                
+                    <Link href='/' className={utilStyles.navItem}>Home</Link>
+                    <Link href='/archive' className={utilStyles.navItem}>Archive</Link>
+                    <Link href='/about' className={utilStyles.navItem}>About</Link>
+                
+                
             </nav>
+            <Script src="/blob.js?foo=${Math.round(
+    Math.random() * 100
+  )}" strategy="beforeInteractive" />
         </>
         
     );

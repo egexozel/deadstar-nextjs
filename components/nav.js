40db67ef0cb42bdc7f89602827c0b72542009140
Ege Özel <egexozel@gmail.com>
2023-02-26 20:54:12 +0300

    updated nav

--- components/nav.js
+++ components/nav.js
@@ -3,12 +3,14 @@ import utilStyles from '../styles/utils.module.css';
 
 const Nav = () => {
     return (
-        <nav className={utilStyles.navbar}>
-            <Link href='/' className={utilStyles.navItem}>Home</Link>
-            <Link href='/archive' className={utilStyles.navItem}>Archive</Link>
-            <Link href='/works' className={utilStyles.navItem}>Works</Link>
-            <Link href='/about' className={utilStyles.navItem}>About</Link>
-        </nav>
+        <>
+            <nav className={utilStyles.navbar}>
+                <Link href='/' className={utilStyles.navItem}>Home</Link>
+                <Link href='/archive' className={utilStyles.navItem}>Archive</Link>
+                <Link href='/about' className={utilStyles.navItem}>About</Link>
+            </nav>
+        </>
+        
     );
 };
 
